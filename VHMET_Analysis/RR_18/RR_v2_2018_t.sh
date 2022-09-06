185a448664d8cdddbacb1af2de3a064f4f00308c
Xingfu <iceeric02@gmail.com>
2022-09-06 09:52:20 +0200

    Input setting backup.

--- VHMET_Analysis/RR_18/RR_v2_2018_t.sh
+++ VHMET_Analysis/RR_18/RR_v2_2018_t.sh
@@ -1,7 +1,7 @@
-outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d1/CMSSW_10_6_8/src/flashgg/VHMET_Analysis/RR_18/Output_file_MCSet1_ZHAC2"
-storedir="/eos/user/x/xisu/Work_files/VH_AnomalousCoupling/Output_File_RR_18/RR_18_v2_d1/Output_file_MCSet1_ZHAC2"
+outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d1/CMSSW_10_6_8/src/flashgg/VHMET_Analysis/RR_18/Output_file_MCSet1_WHAC"
+storedir="/eos/user/x/xisu/Work_files/VH_AnomalousCoupling/Output_File_RR_18/RR_18_v2_d1/Output_file_MCSet1_WHAC"
 
-fggRunJobs.py --load RR_v2_2018_MCSet1_Z_AC.json \
+fggRunJobs.py --load RR_v2_2018_MCSet1_W_AC.json \
               -d $outdir --stage-to $storedir \
               -n 50 \
               -q testmatch \
