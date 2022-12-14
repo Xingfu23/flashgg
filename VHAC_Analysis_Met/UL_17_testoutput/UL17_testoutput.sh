6207262364ddfa643b77ac9a7da78c40abda54fb
Xingfu <iceeric02@gmail.com>
2022-12-14 16:51:26 +0100

    updatae test setting

--- VHAC_Analysis_Met/UL_17_testoutput/UL17_testoutput.sh
+++ VHAC_Analysis_Met/UL_17_testoutput/UL17_testoutput.sh
@@ -1,13 +1,13 @@
-outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d2/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17_testoutput/testoutput"
-storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/testoutput_latetst"
+outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d2/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17_testoutput/testoutput_zh125_2"
+storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/testoutput_latetst_zh125_2"
 
-fggRunJobs.py --load UL_runII_v1_2017.json \
+fggRunJobs.py --load UL17_zh125.json \
               -d $outdir --stage-to $storedir \
               -n 50 \
               -q testmatch \
               --no-use-tarball \
               --no-copy-proxy -D -P \
-              -x cmsRun workspaceStd.py maxEvents=300 \
+              -x cmsRun workspaceStd.py maxEvents=1000 \
                                         doStageOne=True \
                                         doSystematics=False \
                                         dumpWorkspace=False \
