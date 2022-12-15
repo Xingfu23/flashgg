d64fb7ff84dbcc88bb8f021f11e402b7924e246e
Xingfu <iceeric02@gmail.com>
2022-12-15 15:35:31 +0100

    Modify setting and output files

--- VHAC_Analysis_Met/UL_17_testoutput/UL17_testoutput.sh
+++ VHAC_Analysis_Met/UL_17_testoutput/UL17_testoutput.sh
@@ -1,5 +1,5 @@
-outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d2/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17_testoutput/testoutput_zh125_2"
-storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/testoutput_latetst_zh125_2"
+outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d2/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17_testoutput/testoutput_zh125"
+storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/testoutput_latetst_zh125"
 
 fggRunJobs.py --load UL17_zh125.json \
               -d $outdir --stage-to $storedir \
@@ -7,7 +7,7 @@ fggRunJobs.py --load UL17_zh125.json \
               -q testmatch \
               --no-use-tarball \
               --no-copy-proxy -D -P \
-              -x cmsRun workspaceStd.py maxEvents=1000 \
+              -x cmsRun workspaceStd.py maxEvents=5000 \
                                         doStageOne=True \
                                         doSystematics=False \
                                         dumpWorkspace=False \
