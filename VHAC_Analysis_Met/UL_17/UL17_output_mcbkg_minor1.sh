dd40efc5271e0caef06abd6baf032c23cbee1238
Xingfu <iceeric02@gmail.com>
2023-03-09 03:23:40 +0100

    change setting file

--- VHAC_Analysis_Met/UL_17/UL17_output_mcbkg_minor1.sh
+++ VHAC_Analysis_Met/UL_17/UL17_output_mcbkg_minor1.sh
@@ -1,13 +1,13 @@
-outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d2/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17/UL17_minorbkgtop_test"
-storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/UL_dataset/UL_17/UL17_minorbkgtop_test"
+outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_d2/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17/UL17_minorbkg1"
+storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/UL_dataset/UL_17/UL17_minorbkg1"
 
-fggRunJobs.py --load UL17_minorbkgtop.json \
+fggRunJobs.py --load UL17_minorbkg1.json \
               -d $outdir --stage-to $storedir \
               -n 50 \
               -q testmatch \
               --no-use-tarball \
               --no-copy-proxy -D -P \
-              -x cmsRun workspaceStd.py maxEvents=100 \
+              -x cmsRun workspaceStd.py maxEvents=-1 \
                                         doStageOne=True \
                                         doSystematics=False \
                                         dumpWorkspace=False \
