54d291389950b83709a8358c6eea5c8d6104b449
Xingfu <iceeric02@gmail.com>
2023-07-27 11:29:16 +0200

    Upload test setting

--- VHAC_Analysis_Met/UL_17/UL17_vh.sh
+++ VHAC_Analysis_Met/UL_17/UL17_vh.sh
@@ -1,5 +1,5 @@
-outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_acvhlep/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17/UL17_vh_sys_test"
-storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/UL_dataset_aclep/UL_17/UL17_vh_sys_test"
+outdir="/afs/cern.ch/user/x/xisu/private/local/Analysis_workspace/VH_AC_tmp1/flashgg_acvhlep/CMSSW_10_6_29/src/flashgg/VHAC_Analysis_Met/UL_17/UL17_vh_newac_test2"
+storedir="/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/UL_dataset_aclep/UL_17/UL17_vh_newac_test2"
 
 fggRunJobs.py --load json/UL17_vh125.json \
               -d $outdir --stage-to $storedir \
@@ -7,10 +7,13 @@ fggRunJobs.py --load json/UL17_vh125.json \
               -q testmatch \
               --no-use-tarball \
               --no-copy-proxy -D -P \
-              -x cmsRun workspaceVbf.py maxEvents=1000 \
-                                        doStageOne=True \
-                                        doSystematics=True \
-                                        doPdfWeights=True \
+              -x cmsRun workspaceVbf.py maxEvents=10000 \
+                                        anomalousCouplings=True \
+                                        doStageOne=False \
+                                        doSystematics=False \
+                                        doPdfWeights=False \
                                         dumpWorkspace=False \
                                         dumpTrees=True \
-                                        disableJEC=True \
+                                        disableJEC=False \
+                                        dumpLHE=False \
+                                        melaEFT=False \
